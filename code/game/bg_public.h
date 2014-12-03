d50fcf8787aaab6c9f68e57f15bf6759ee160ca6
ChicagoSaveTheDarknessDev <JackKucan+chicagogithub@gmail.com>
2014-12-03 07:47:50 -0500

    Make armor protect against 100% of damage

--- code/game/bg_public.h
+++ code/game/bg_public.h
@@ -49,7 +49,7 @@ Suite 120, Rockville, Maryland 20850 USA.
 
 #define	DEFAULT_GRAVITY		800
 #define	GIB_HEALTH			-40
-#define	ARMOR_PROTECTION	0.66
+#define	ARMOR_PROTECTION	1.00
 
 #define	MAX_LOCATIONS		64
 
